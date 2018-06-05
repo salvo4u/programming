1b78b37a64ca67861fd14af510115bbd75b17418
anshulgolu123 <anshulgolu123@gmail.com>
2018-06-05 14:55:54 +0530

    Coin Change problem

--- Programming1/src/hackerank/CoinChange.java
+++ Programming1/src/hackerank/CoinChange.java
@@ -1,44 +1,42 @@
 package hackerank;
 //https://www.hackerrank.com/challenges/ctci-coin-change/problem
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class CoinChange {
-	public static int methodCoinChange(HashSet<Integer> set,int money){
-		int count = 0;
-		if(set.contains(money))
-			return 1;
-		Iterator<Integer> iter = set.iterator();
-		while (iter.hasNext()) {
-			Integer m = iter.next();
-			if(m>money)
-				break;
-			count = count + methodCoinChange(set,m) + methodCoinChange(set,money - m);
-		}
-		return count;
-	}
-	
+	private static int mat[][];
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         String[] nm = scanner.nextLine().split(" ");
-
         int n = Integer.parseInt(nm[0]);
-
         int m = Integer.parseInt(nm[1]);
-
         int[] coins = new int[m];
-
         String[] coinsItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        HashSet<Integer> set = new HashSet<Integer>();
         for (int i = 0; i < m; i++) {
             int coinsItem = Integer.parseInt(coinsItems[i]);
             coins[i] = coinsItem;
-            set.add(coins[i]);
         }
-        System.out.println(methodCoinChange(set,n));
+        mat = new int[m][n+1];
+        for(int i=0;i<m;i++){
+        	for(int j=0;j<=n;j++){
+        		if(i==0 && j%coins[i]==0){
+        			mat[i][j] = 1;
+        			continue;
+        		}
+        		if(j==0){
+        			mat[i][j] = 1;
+        			continue;
+        		}
+        		if(j-coins[i]<0 && i-1>=0)
+        		{
+        			mat[i][j] = mat[i-1][j];
+        		}
+        		if(i-1>=0 && j-coins[i]>=0)
+        			mat[i][j] = mat[i-1][j] + mat[i][j-coins[i]]; 
+        	}
+        }        
+        System.out.println(mat[m-1][n]);
         scanner.close();
     }
 }
